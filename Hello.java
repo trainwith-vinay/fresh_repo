3f9f0395577382601edd0dabbfbc89f075ed79d2
vinay <vinay@demo.com>
2021-08-25 16:48:28 +0530

    from fresh repo

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("this is fresh repo from my local");
+   }
+}
